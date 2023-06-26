dc3692494616d583ef460a9c9217f3575926cb8d
mazv-arshad <131137167+mazv-arshad@users.noreply.github.com>
2023-06-26 11:22:26 +0530

    Update streamlit_app.py
    #Adding Fruityvice data into table

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,5 +26,9 @@ import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response.json())
               
+# write your own comment -what does the next line do? #Adding Fruityvice data into table
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# write your own comment - what does this do?
+streamlit.dataframe(fruityvice_normalized)
               
               
